16b44e33a4b523492643ebda4d39b1487187ea2d
rajveermalviya <rajveer0malviya@gmail.com>
2018-07-19 11:10:33 +0530

    improved testing

--- test.py
+++ test.py
@@ -11,32 +11,41 @@ _, _, _, indexToString, stringToIndex = load_data()
 
 
 model = load_model(os.path.join(os.getcwd(), 'model', 'model.h5'))
-NUMBER_OF_PREDICTIONS = 10
 
 
-def predict_next_word(string, verbose=True):
+
+def predict_next_word(string, verbose=True, NUMBER_OF_PREDICTIONS= 10):
+
   ques_bool = False
   idx, ques_bool = string_to_indexes(string.split(), ques_bool)
+
   if len(idx) >= 3:
-    idx = idx[-3:]
     if verbose:
-      print('last 3 words\t:', idx)
-    prediction = model.predict([[idx]])
-    best_predictions = list()
+      print('\nindexes of last 3 words\t:', idx[-3:])
+    prediction = model.predict([[idx[-3:]]])
+
+    best_predictions = []
+
     for _ in range(NUMBER_OF_PREDICTIONS):
       argmax_idx = argmax(prediction[:, CONFIG.num_steps - 1, :])
       best_predictions.append(argmax_idx)
       prediction[:, CONFIG.num_steps - 1, argmax_idx] = 0.0
+
     if verbose:
-      print('prediction\t:', best_predictions)
+      print('\nprediction indexes\t:', best_predictions)
     converted_string = indexes_to_string(best_predictions, ques_bool)
-    return string + ' ' + converted_string[0]
+    sentences = []
+
+    for word in converted_string:
+      sentences.append(string + ' ' + word)
+    return sentences
   else:
-    print('Please enter atleast 3 words.')
+    print('\n\nPlease enter atleast 3 words.\n')
 
 
 def string_to_indexes(array_of_string, ques_bool):
-  array_of_indexes = list()
+  array_of_indexes = []
+
   for word in array_of_string:
     if word == '<rare word>':
       word = '<unk>'
@@ -44,6 +53,7 @@ def string_to_indexes(array_of_string, ques_bool):
       word = '<eos>'
     if word == 'what' or word == 'why' or word == 'who' or word == 'how' or word == 'whose' or word == 'when' or word == 'which' or word == 'where':
       ques_bool = True
+
     try:
       array_of_indexes.append(stringToIndex[word])
     except:
@@ -55,7 +65,8 @@ def string_to_indexes(array_of_string, ques_bool):
 
 
 def indexes_to_string(array_of_indexes, ques_bool):
-  array_of_strings = list()
+  array_of_strings = []
+
   for index in array_of_indexes:
     word = indexToString[index]
     if word == '<eos>':
@@ -70,13 +81,11 @@ def indexes_to_string(array_of_indexes, ques_bool):
   return array_of_strings
 
 
-def create_string(string='welcome to new'):
-  __string = predict_next_word(string, verbose=False)
-  for _ in range(NUMBER_OF_PREDICTIONS):
-    __string = predict_next_word(__string, verbose=False)
-  print('\n\nGenerated String:\t', __string, '\n')
-
-
 while True:
-  input_string = input('\n\nEnter atleast 3 words: \n')
-  predict_next_word(input_string)
+  sentences = predict_next_word(string=input('\n\nEnter atleast 3 words: \n'), NUMBER_OF_PREDICTIONS=10)
+  print('\n')
+  if sentences != None:
+    count = 0
+    for sentence in sentences:
+      count += 1
+      print(count, '-',sentence)
