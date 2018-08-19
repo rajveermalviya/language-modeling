ae3560a345d71d4e1e944f5a2323d3f8b168016d
rajveermalviya <rajveer0malviya@gmail.com>
2018-08-19 13:46:57 +0530

    updated the model

--- keras_model.py
+++ keras_model.py
@@ -6,9 +6,24 @@ from tensorflow.keras.models import Sequential
 
 def create_model(total_words, hidden_size, num_steps, optimizer='adam'):
   model = Sequential()
+
+  # Embedding layer
   model.add(Embedding(total_words, hidden_size, input_length=num_steps))
+
+  # 4 LSTM layers
   model.add(LSTM(units=hidden_size, return_sequences=True))
   model.add(LSTM(units=hidden_size, return_sequences=True))
+  model.add(LSTM(units=hidden_size, return_sequences=True))
+  model.add(LSTM(units=hidden_size, return_sequences=True))
+
+  # Fully Connected layer
+  model.add(TimeDistributed(Dense(1024)))
+  model.add(Activation('relu'))
+  model.add(Dropout(0.3,seed=0.2))
+  model.add(TimeDistributed(Dense(512)))
+  model.add(Activation('relu'))
+
+  # Output Layer
   model.add(TimeDistributed(Dense(total_words)))
   model.add(Activation('softmax'))
 
